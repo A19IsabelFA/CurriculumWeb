e21e5fc0b3f62116cfb9e13b48c6c13962f66485
A19IsabelFA <a19isabelfa@iessanclemente.net>
2021-05-19 15:25:47 +0200

    MOD - generacion del footer automatico

--- assets/js/eventos.js
+++ assets/js/eventos.js
@@ -1,4 +1,13 @@
-  // boton menu
+ // generacion del footer
+
+let pie = `<div class="container">
+<p> &copy; 2021 - Isabel Ferreiro Abeijón - CV</p>
+<a href="https://github.com/A19IsabelFA/CurriculumWeb" target="_blank"><i class="fab fa-github"></i></a>
+</div>`;
+ 
+document.querySelector('footer').innerHTML = pie;
+
+ // boton menu
   let btnmenumovil = document.getElementById('menuMovil');
   let menu = document.getElementById('menu');
   btnmenumovil.addEventListener('click', () => {
